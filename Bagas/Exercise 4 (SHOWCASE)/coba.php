048203b76febbffe89866ea9bf27248b3bb2d316
bagaspratama <bpratama369@gmail.com>
2018-10-17 14:03:11 +0700

    Add files via upload

---
+++ Bagas/Exercise 4 (SHOWCASE)/coba.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>master node</title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+	<!-- <link rel="stylesheet" type="text/css" href="style3.css"> -->
+</head>
+<body background="bg.png">
+	
+	<!-- WRITE YOUR CODE HERE -->
+<nav class="navbar navbar-expand-lg navbar-dark bg_nav fixed-top ">
+  <div class="container">
+    <a class="navbar-brand" href="#">Excercise 4</a>
+  	<div style="text-align: right;"">
+  	<div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link display-4 mt-3" href="#">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link display-4 mt-3 mx-3" href="#">PLAN</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link display-4 mt-3" href="#">SEND</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+  </div>
+</nav>
+
+
+	<!-- END-WRITE YOUR CODE HERE -->
+
+	<script src="assets/jquery/js/jquery-3.3.1.slim.min.js"></script>
+	<script src="assets/popper/js/popper.min.js"></script>
+	<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+</body>
+</html>
